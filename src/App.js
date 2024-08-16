7aece85a8d4121e81fc2a856c450b7030b026493
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-16 13:46:37 +0400

    debug

--- src/App.js
+++ src/App.js
@@ -14,13 +14,17 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
 
   const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = currentCity === 'See all cities'
-      ? allEvents
-      : allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  }
+    try {
+      const allEvents = await getEvents();
+      const filteredEvents = currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter(event => event.location === currentCity)
+      setEvents(filteredEvents.slice(0, currentNOE));
+      setAllLocations(extractLocations(allEvents));
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    }
+  };
 
   useEffect(() => {
     fetchData();
