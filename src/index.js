054920418586fa34a99f20df44f687bdff98e0af
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-16 12:01:06 +0400

    Atatus installation setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+/* eslint-disable semi */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+import * as atatus from 'atatus-spa';
+atatus.config('fdadd6c1042d4ea09db52ef6d54ba76e').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,3 +25,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
